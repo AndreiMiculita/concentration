74309736028ba84856fef21eaa5f81197a0226ed
A. Miculita <s4161947@ai178130.local>
2020-02-13 15:14:28 +0100

    After first episode

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃","👻", "🎃","👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of:sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            print("cardnumber = \(cardNumber)")
+        } else {
+            print("not in cardbuttons")
+        }
+//        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
 }
 
